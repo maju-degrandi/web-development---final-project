129f2b507f5988c95593c17a93bf49e748afe4d1
ThierryAra <thierry_ara@usp.br>
2023-05-31 00:16:07 -0300

    Updated component name

---
+++ src/components/Footer/footerList.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import NavLink from '../NavLink';
+
+import '../../styles/footer.css'
+
+export const FooterList = ({title, className, links}) => {
+    const items = Object.entries(links);
+    return (
+        <div className={className}>
+            {title && (<h6>{title}</h6>)}
+            <ul className="footer-list-wrap-ul">
+                {items.map(([text, link], i) => (
+                    <NavLink key={i} href={link} text={text} liClassName=''/>
+                ))}
+            </ul>
+        </div>
+    );
+};
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import '../../styles/footer.css'
+
+import logoImg from '../../assets/logo.png'
+import { FooterList } from './footerList';
+import NavLink from '../NavLink';
+
+export const Footer = () => {
+    return (
+        <div id='footer'>
+            <div className="footer-info" >
+                <a href="/" id="footer-img">
+                    <figure >
+                        <img className="footer-logo" src={logoImg} alt='Logo da loja'/>
+                    </figure>
+                </a>
+                
+                <nav id='footer-list'>
+                    <FooterList 
+                        title='Customer Support'
+                        className="footer-list-wrap"
+                        links={{
+                            'Shipping': '/',
+                            'Free Returns': '/',
+                            'Track Your Order': '/',
+                            'Git Cards': '/',
+                        }
+                    }
+                    />
+                    
+                    <FooterList 
+                        title='Customer Services'
+                        className="footer-list-wrap"
+                        links={{
+                            'Shipping': '/',
+                            'Free Returns': '/',
+                            'Track Your Order': '/',
+                            'Git Cards': '/',
+                        }
+                    }
+                    />
+                    
+                    <FooterList 
+                        title='About Us'
+                        className="footer-list-wrap"
+                        links={{
+                            'Shipping': '/',
+                            'Free Returns': '/',
+                            'Track Your Order': '/',
+                            'Git Cards': '/',
+                            'About Us': '/',
+                        }
+                    }
+                    />
+                </nav>
+                
+                <ul className="footer-bar">
+                    <NavLink href='/' aClassName='fa fa-telegram' text=''/>
+                    <NavLink href='/' aClassName='fa fa-instagram' text=''/>
+                    <NavLink href='/' aClassName='fa fa-twitter' text=''/>
+                    <NavLink href='/' aClassName='fa fa-facebook' text=''/>
+                    <NavLink href='/' aClassName='fa fa-pinterest' text=''/>
+                </ul>
+            </div>
+        </div>
+    );
+};

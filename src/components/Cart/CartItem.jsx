fece66504442fde0ed5664954d48a29b9f63dcdb
Rebeca <rebeca.vcarvalho78@gmail.com>
2023-06-10 15:39:54 -0300

    Fixing cart responsiveness

--- src/components/Cart/CartItem.jsx
+++ src/components/Cart/CartItem.jsx
@@ -20,8 +20,8 @@ export const CartItem = ({ updateCart, item, cart, blocked }) => {
 
     return(
         <li key={item.id} className='cart-item'>
-            <div className="item-cover">
-                <img className='cart-item-cover' src={item.cover} alt={`${item.name} cover`}/>  
+            <div className="card-item-cover">
+                <img className='item-cover' src={item.cover} alt={`${item.name} cover`}/>  
             </div>
             <span className="cart-item-name">{item.name}<br/>$ {item.price}</span>
             <div className="cart-shop">
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -4,26 +4,47 @@ import { Card } from "../components/Card";
 import { CartList } from '../components/Cart/CartList';
 import { Button } from '../components/Button';
 import { Link } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export const PageCart = ({updateCart, cart, setCart}) => {
+    const [qttItems, setQttItens] = useState([]);
+
+    useEffect(()=>{
+        let qtt = 0;
+        cart.map((item) => {
+            if(item.qtt) qtt += item.qtt;
+        })
+        
+        setQttItens(qtt);
+    }, [cart]);
 
     return (
         
-        <div id='wrapper' className='cart'>
+        <div id='wrapper' className='checkout'>
             <div id='checkout-title'>
                 <h1>Cart</h1>
             </div>
-            <Card>
-                <CartList updateCart={updateCart} cart={cart} setCart={setCart} blocked={false}/>
-            </Card>
+            <div id='checkout-main'>
+                <div className='checkout-items'>
+                    <CartList updateCart={updateCart} cart={cart} setCart={setCart} blocked={false}/>
+                </div>
+            </div>
             
             <div id='checkout-resume'>
-                <h2>Subtotal: {cart[0].subtotal}</h2>
-                <Link to='/checkout'>
-                    <Button text="Checkout"/>
-
-                </Link>
+            <div className='flex'>
+                    <p className='type'>Items({qttItems})</p>
+                    <p className='price'>R$
+                        {
+                            Number.parseFloat(cart[0].subtotal).toFixed(2)
+                        }
+                    </p>
+                </div>
+                
+                <div className='flex full-line'>
+                    <Link to='/checkout'>
+                        <Button text="Checkout"/>
+                    </Link>
+                </div>
             </div>
         </div>
     );

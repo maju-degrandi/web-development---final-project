706b12c586eaa1b23582f0d3a74cd90d8c25444d
ThierryAra <thierry_ara@usp.br>
2023-06-10 15:45:52 -0300

    Some styles edited

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,20 +8,24 @@ import { Link } from 'react-router-dom';
 export const PageLogin = () => {
     return (
         <>
-        <img id="background-image1" src={backimage}/>
+        <figure className='background-login'>
+            <img id="background-image1" src={backimage}/>
+        </figure>
+        
+            
         <div className="basic-form">
-                <h1 className="title">Login</h1>
-                <form className="basic-form-form">
-                    <Input label="Email: " input="email" type="email"/>
-                    <Input label="Senha: " input="senha" type="password"/>
-                    <Link className='basic-link' to={"/profile"}>
-                        <Button text="LOGIN" className="filled-button basic-button" type="submit" />
-                    </Link>
-                </form>
-                <div className="links-loginscreen">
-                    <Link to={'/register'}>Don't have an account yet?</Link><br/>
-                    <Link to={'#'}>Forgot your password?</Link>
-                </div>
+            <h1 className="title">Login</h1>
+            <form className="basic-form-form">
+                <Input label="Email: " input="email" type="email"/>
+                <Input label="Senha: " input="senha" type="password"/>
+                <Link className='basic-link' to={"/profile"}>
+                    <Button text="LOGIN" className="filled-button basic-button" type="submit" />
+                </Link>
+            </form>
+            <div className="links-loginscreen">
+                <Link to={'/register'}>Don't have an account yet?</Link><br/>
+                <Link to={'#'}>Forgot your password?</Link>
+            </div>
          </div>
         </>
     );

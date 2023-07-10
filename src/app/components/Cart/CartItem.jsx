d6043529199593982f6afd8d7e66bca8a450be47
ThierryAra <thierry_ara@usp.br>
2023-07-10 00:55:09 -0300

    Removing input number editing with writing

--- src/app/components/Cart/CartItem.jsx
+++ src/app/components/Cart/CartItem.jsx
@@ -26,7 +26,9 @@ export const CartItem = ({ updateCart, item, cart, blocked }) => {
                     <img className='item-cover' src={item.cover} alt={`${item.name} cover`}/>  
                 </div>
                 <div className="title-price">
-                    <span className="cart-item-name" data-test_id={'Item ' + 1} >{item.name} </span> 
+                    <span className="cart-item-name" data-test_id={'Item ' + 1} >
+                        { item.name }
+                    </span> 
                     <br/>
                     <span className="cart-item-name">$ {item.price*item.qtt}</span>
                 </div>
@@ -36,12 +38,18 @@ export const CartItem = ({ updateCart, item, cart, blocked }) => {
                 <div className="cart-shop">
                     <label htmlFor="quant">QUANTITY:</label>
                     { !blocked ? 
-                        <input type="number" className="number" name="quant" min={1} max={item.stock} value={item.qtt} onChange={handleChangeQuantity}></input>
+                        <input type="number" className="number" name="quant" min={1} 
+                            onKeyDown={(e) => e.preventDefault()}
+                            onWheel={(e) => e.preventDefault()}
+                            max={item.stock} value={item.qtt} onChange={handleChangeQuantity}>    
+                        </input>
                         :
-                        <Input className='disable' type={'number'} name={'quant'} min={1} max={item.stock} value={item.qtt}  onChange={handleChangeQuantity}/>
+                        <Input className='disable' type={'number'} name={'quant'} min={1} 
+                        max={item.stock} value={item.qtt}  onChange={handleChangeQuantity}/>
                     }
                 </div>
-                {!blocked && <Button text="Delete" onClick={handleDeleteItem} className="filled-button" />}
+                {!blocked && 
+                <Button text="Delete" onClick={handleDeleteItem} className="filled-button" />}
             </div>
         </li>
     );
